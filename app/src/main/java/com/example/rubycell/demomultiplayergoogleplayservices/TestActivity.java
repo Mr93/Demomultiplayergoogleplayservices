54cc8c76a7124061b47ec0870448e03bf79ed5da
anhdn <anh.dn@rubycell.com>
2016-10-18 16:54:09 +0700

    check host by ping

--- app/src/main/java/com/example/rubycell/demomultiplayergoogleplayservices/TestActivity.java
+++ app/src/main/java/com/example/rubycell/demomultiplayergoogleplayservices/TestActivity.java
@@ -6,46 +6,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.buddy.sdk.Buddy;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Result;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.games.Games;
 import com.google.android.gms.games.GamesActivityResultCodes;
 import com.google.android.gms.games.GamesStatusCodes;
 import com.google.android.gms.games.multiplayer.Invitation;
-import com.google.android.gms.games.multiplayer.Invitations;
 import com.google.android.gms.games.multiplayer.Multiplayer;
 import com.google.android.gms.games.multiplayer.OnInvitationReceivedListener;
 import com.google.android.gms.games.multiplayer.Participant;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
-import com.google.android.gms.games.multiplayer.realtime.RealTimeMultiplayer;
 import com.google.android.gms.games.multiplayer.realtime.Room;
 import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
 import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
 import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
-import com.google.example.games.basegameutils.BaseGameActivity;
 import com.google.example.games.basegameutils.BaseGameUtils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 
 /**
@@ -86,6 +81,8 @@ public class TestActivity extends AppCompatActivity
 	Room mRoom;
 	Queue<String> queueSendData;
 	ArrayList<Integer> listPing;
+	ArrayList<Integer> listTotalPing;
+	int hostIndex = -1;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -144,25 +141,38 @@ public class TestActivity extends AppCompatActivity
 
 
 				ArrayList<String> invitees = new ArrayList<>();
-				invitees.add("g08579370587522082113");
+				invitees.add("109513629537103738692");
 
-				Bundle am = RoomConfig.createAutoMatchCriteria(1,7, 0);
+				Bundle am = RoomConfig.createAutoMatchCriteria(6,7, 0);
 
 				//create the room and specify a variant if appropriate
 				RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
-				roomConfigBuilder.addPlayersToInvite(invitees);
-				Log.d(TAG, "onActivityResult: ");
+				//roomConfigBuilder.addPlayersToInvite(invitees);
+				roomConfigBuilder.setAutoMatchCriteria(am);
+				roomConfigBuilder.setVariant(1);
 				RoomConfig roomConfig = roomConfigBuilder.build();
-				Games.RealTimeMultiplayer .create(mGoogleApiClient, roomConfig);
+				Log.d(TAG, "onActivityResult: invitationId " + roomConfig.getInvitationId());
+				Games.RealTimeMultiplayer.create(mGoogleApiClient, roomConfig);
 
 				//prevent screen from sleeping during handshake
 				getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 				break;
 			case R.id.btn_show_join:
+				am = RoomConfig.createAutoMatchCriteria(6,7, 0);
 
+				//create the room and specify a variant if appropriate
+				roomConfigBuilder = makeBasicRoomConfigBuilder();
+				//roomConfigBuilder.addPlayersToInvite(invitees);
+				roomConfigBuilder.setAutoMatchCriteria(am);
+				roomConfigBuilder.setVariant(1);
+				Log.d(TAG, "onActivityResult: ");
+				roomConfig = roomConfigBuilder.build();
 
+				Games.RealTimeMultiplayer.join(mGoogleApiClient, roomConfig);
 
+				//prevent screen from sleeping during handshake
+				getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 				Log.d(TAG, "onClick: Id" + Games.Players.getCurrentPlayerId(mGoogleApiClient));
 				break;
 			case R.id.btn_invite:
@@ -199,15 +209,20 @@ public class TestActivity extends AppCompatActivity
 				}
 				break;
 			case R.id.buttonSend:
-				sentData(-1, ((EditText) findViewById(R.id.editText)).getText().toString(), myIndex);
+				getPing();
+				getPinged = true;
+
+				/*sentData(-1, ((EditText) findViewById(R.id.editText)).getText().toString(), myIndex);
 				TextView textView = (TextView) findViewById(R.id.textView);
 				textView.setText(textView.getText() + "\n Player" + myIndex + ": "
 						+ ((EditText) findViewById(R.id.editText)).getText().toString());
-				((EditText) findViewById(R.id.editText)).setText("");
+				((EditText) findViewById(R.id.editText)).setText("");*/
 			default:
 				break;
 		}
 	}
+
+	boolean getPinged = false;
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -263,7 +278,9 @@ public class TestActivity extends AppCompatActivity
 				getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 			} else if (resultCode == GamesActivityResultCodes.RESULT_LEFT_ROOM) {
 				// player wants to leave the room.
-				Games.RealTimeMultiplayer.leave(mGoogleApiClient, null, mRoomId);
+				if(mRoomId != null){
+					Games.RealTimeMultiplayer.leave(mGoogleApiClient, this, mRoomId);
+				}
 				getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 			}
 		}
@@ -391,34 +408,77 @@ public class TestActivity extends AppCompatActivity
 	}
 
 	boolean mWaitingRoomFinishedFromCode = false;
+	int totalPingHad = 0;
 
 	@Override
 	public void onRealTimeMessageReceived(RealTimeMessage realTimeMessage) {
+		TextView textView = (TextView) findViewById(R.id.textView);
+		textView.setMovementMethod(new ScrollingMovementMethod());
 		{
 			byte[] b = realTimeMessage.getMessageData();
 			String temp = new String(b, Charset.forName("UTF-8"));
 			Log.d(TAG, "onRealTimeMessageReceived: " + temp);
 			if (temp.length() > 3) {
-				String prefix = temp.substring(0,3);
-				if("chat".equalsIgnoreCase(prefix)){
-					TextView textView = (TextView) findViewById(R.id.textView);
+				if(temp.contains("myping")){
+					if(!getPinged){
+						getPing();
+					}
+					responsePing(temp.replace("myping",""), realTimeMessage.getSenderParticipantId());
+				}else if(temp.contains("responsePing")){
+					int indexOfRespone = Integer.valueOf(temp.substring(0,1));
+					int ping = (int) (System.currentTimeMillis() - Long.valueOf(temp.replace("responsePing","").substring(1)));
+					Log.d(TAG, "onRealTimeMessageReceived: index: " + indexOfRespone);
+					Log.d(TAG, "onRealTimeMessageReceived: ping: " + ping + "ms");
+					listPing.add(ping/2);
+					textView.setText(textView.getText() +"\n" + "index " + indexOfRespone +", ping :" + (ping/2) );
+					if(listPing.size() == mParticipants.size()-1){
+						int valueTmp = 0;
+						for(int i = 0; i < listPing.size(); i++){
+							valueTmp = valueTmp + listPing.get(i);
+						}
+						listTotalPing.set(myIndex, valueTmp);
+						totalPingHad++;
+						if(totalPingHad == mParticipants.size()){
+							int valueMin = listTotalPing.get(0);
+							for(int i = 0; i < listTotalPing.size(); i++){
+								if(listTotalPing.get(i) <= valueMin){
+									valueMin = listTotalPing.get(i);
+									hostIndex = i;
+								}
+							}
+							textView.setText(textView.getText() +"\n" + "Host index " + hostIndex);
+						}
+						sentMyTotalPing(valueTmp);
+						textView.setText(textView.getText() +"\n" + "my total ping " + valueTmp);
+					}
+				}else if(temp.contains("mytotalping")){
+					listTotalPing.set(Integer.valueOf(temp.substring(0,1)), Integer.valueOf(temp.replace("mytotalping","")
+							.substring(1)));
+					Log.d(TAG, "onRealTimeMessageReceived: " + temp);
+					Log.d(TAG, "onRealTimeMessageReceived: " + Integer.valueOf(temp.substring(0,1)));
+					Log.d(TAG, "onRealTimeMessageReceived: " + Integer.valueOf(temp.replace("mytotalping","")
+							.substring(1)));
+					totalPingHad++;
+					Log.d(TAG, "onRealTimeMessageReceived: tempTotal " + totalPingHad);
+					if(totalPingHad == mParticipants.size()){
+						int valueMin = listTotalPing.get(0);
+						for(int i = 0; i < listTotalPing.size(); i++){
+							if(listTotalPing.get(i) <= valueMin){
+								valueMin = listTotalPing.get(i);
+								hostIndex = i;
+							}
+						}
+						textView.setText(textView.getText() +"\n" + "Host index " + hostIndex);
+					}
+				}else if (temp.contains("chat")){
+					textView = (TextView) findViewById(R.id.textView);
 					textView.setText(textView.getText() + "\n" + getPlayerName(realTimeMessage.getSenderParticipantId())
 							+ ": " + temp.substring(4));
-				}else {
-					if(myIndex != 0){
-						responePing(temp, realTimeMessage.getSenderParticipantId());
-					}else {
-						int indexOfRespone = Integer.valueOf(temp.substring(0,1));
-						int ping = (int) (System.currentTimeMillis() - Long.valueOf(temp.substring(5)));
-						Log.d(TAG, "onRealTimeMessageReceived: index: " + indexOfRespone);
-						Log.d(TAG, "onRealTimeMessageReceived: ping: " + ping + "ms");
-						listPing.set(indexOfRespone, ping/2);
-					}
 				}
 			} else {
 				int dataIndex = Integer.valueOf(temp.substring(0, 1)) ;
 				int dataPosition = Integer.valueOf(temp.substring(1)) ;
-				if(myIndex == 0){
+				if(myIndex == hostIndex){
 					Log.d(TAG, "onRealTimeMessageReceived: sent to client");
 					sentData(dataPosition, null, dataIndex);
 				}else {
@@ -478,7 +538,7 @@ public class TestActivity extends AppCompatActivity
 	@Override
 	public void onDisconnectedFromRoom(Room room) {
 		// leave the room
-		Games.RealTimeMultiplayer.leave(mGoogleApiClient, null, mRoomId);
+		Games.RealTimeMultiplayer.leave(mGoogleApiClient, this, mRoomId);
 
 		// clear the flag that keeps the screen on
 		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -504,7 +564,7 @@ public class TestActivity extends AppCompatActivity
 			// the game to go on, end the game and leave the room.
 		} else if (shouldCancelGame(room)) {
 			// cancel the game
-			Games.RealTimeMultiplayer.leave(mGoogleApiClient, null, mRoomId);
+			Games.RealTimeMultiplayer.leave(mGoogleApiClient, this, mRoomId);
 			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		}
 	}
@@ -524,6 +584,8 @@ public class TestActivity extends AppCompatActivity
 		Log.d(TAG, "onRoomCreated: " + i);
 		if(room == null){
 			Log.d(TAG, "onRoomCreated: room null");
+		}else {
+			Log.d(TAG, "onRoomCreated: roomid " + room.getParticipants().size());
 		}
 		if (i != GamesStatusCodes.STATUS_OK) {
 			//ldet screen go to sleep
@@ -531,6 +593,7 @@ public class TestActivity extends AppCompatActivity
 			Toast.makeText(this, "Error create room", Toast.LENGTH_SHORT).show();
 		}else {
 			// get waiting room intent
+
 			Intent intent = Games.RealTimeMultiplayer.getWaitingRoomIntent(mGoogleApiClient, room, MIN_PLAYERS);
 			startActivityForResult(intent, RC_WAITING_ROOM);
 		}
@@ -543,13 +606,14 @@ public class TestActivity extends AppCompatActivity
 			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 			Toast.makeText(this, "Error create room", Toast.LENGTH_SHORT).show();
 		}
+		Log.d(TAG, "onJoinedRoom: " + room.getParticipants().size());
 		Intent intent = Games.RealTimeMultiplayer.getWaitingRoomIntent(mGoogleApiClient, room, MIN_PLAYERS);
 		startActivityForResult(intent, RC_WAITING_ROOM);
 	}
 
 	@Override
 	public void onLeftRoom(int i, String s) {
-
+		Toast.makeText(this, "Player left", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
@@ -559,6 +623,7 @@ public class TestActivity extends AppCompatActivity
 			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 			Toast.makeText(this, "Error create room", Toast.LENGTH_SHORT).show();
 		} else {
+			Log.d(TAG, "onRoomConnected: " + room.getParticipants().size());
 			mRoomId = room.getRoomId();
 			mRoom = room;
 		}
@@ -595,9 +660,10 @@ public class TestActivity extends AppCompatActivity
 		}
 		if (screenId == R.id.screen_game) {
 			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-			mParticipants = mRoom.getParticipants();
 			mMyId = mRoom.getParticipantId(Games.Players.getCurrentPlayerId(mGoogleApiClient));
 			Log.d(TAG, "onCreate: " + mMyId);
+			mParticipants = mRoom.getParticipants();
+			createListPing();
 			getMyIndex();
 			createDefaultValue();
 			gridView = (GridView) findViewById(R.id.gridView);
@@ -611,17 +677,16 @@ public class TestActivity extends AppCompatActivity
 				}
 			});
 			findViewById(R.id.buttonSend).setOnClickListener(this);
-			createListPing();
-			getPing();
+			//getPing();
 		}
 	}
 
 	private void createListPing() {
-		if(myIndex == 0){
-			listPing = new ArrayList<>();
-			for (int i = 0; i < mParticipants.size(); i++){
-				listPing.add(i, 0);
-			}
+		listPing = new ArrayList<>();
+		listTotalPing = new ArrayList<>();
+		for (int i = 0; i < mParticipants.size(); i++){
+			//listPing.add(i, 0);
+			listTotalPing.add(i, 0);
 		}
 	}
 
@@ -634,6 +699,7 @@ public class TestActivity extends AppCompatActivity
 				myIndex++;
 			}
 		}
+		Toast.makeText(this, "My index " + myIndex, Toast.LENGTH_SHORT).show();
 	}
 
 	public String getPlayerName(String id) {
@@ -649,7 +715,16 @@ public class TestActivity extends AppCompatActivity
 	}
 
 	public void getPing(){
-		if(myIndex == 0){
+		/*ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+		ActivityManager activityManager = (ActivityManager)getSystemService(ACTIVITY_SERVICE);
+		activityManager.getMemoryInfo(mi);
+		Log.d(TAG, "getPing: " + mi.availMem/1048576L + " MB");
+		Log.d(TAG, "getPing: CPU " + readUsage());
+		Log.d(TAG, "getPing: Ram: " + getMemoryInfo());
+		Log.d(TAG, "getPing: CPU: " + getCpuInfo());*/
+
+		/*if(myIndex == 0){
+			Toast.makeText(this, "Index 0", Toast.LENGTH_SHORT).show();
 			String data = "ping" + System.currentTimeMillis();
 			byte[] message = data.getBytes(Charset.forName("UTF-8"));
 			for (Participant p : mParticipants) {
@@ -658,15 +733,73 @@ public class TestActivity extends AppCompatActivity
 							mRoom.getRoomId(), p.getParticipantId());
 				}
 			}
+		}*/
+
+		if(totalPingHad == mParticipants.size()){
+			totalPingHad = 0;
+		}
+		String data = "myping" + System.currentTimeMillis();
+		byte[] message = data.getBytes(Charset.forName("UTF-8"));
+		for (Participant p : mParticipants) {
+			if (!p.getParticipantId().equals(mMyId)) {
+				Games.RealTimeMultiplayer.sendUnreliableMessage(mGoogleApiClient, message,
+						mRoom.getRoomId(), p.getParticipantId());
+			}
 		}
 	}
 
-	public void responePing(String data, String idHost){
-		data = myIndex + data;
-		Log.d(TAG, "responePing: " + data);
+	private float readUsage() {
+		try {
+			RandomAccessFile reader = new RandomAccessFile("/proc/stat", "r");
+			String load = reader.readLine();
+
+			String[] toks = load.split(" +");  // Split on one or more spaces
+
+			long idle1 = Long.parseLong(toks[4]);
+			long cpu1 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[5])
+					+ Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+			try {
+				Thread.sleep(360);
+			} catch (Exception e) {}
+
+			reader.seek(0);
+			load = reader.readLine();
+			reader.close();
+
+			toks = load.split(" +");
+
+			long idle2 = Long.parseLong(toks[4]);
+			long cpu2 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[5])
+					+ Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+			return (float)(cpu2 - cpu1) / ((cpu2 + idle2) - (cpu1 + idle1));
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		return 0;
+	}
+
+	public void responsePing(String data, String idHost){
+		data = myIndex + "responsePing" + data;
+		Log.d(TAG, "responsePing: " + data);
 		byte[] message = data.getBytes(Charset.forName("UTF-8"));
 		Games.RealTimeMultiplayer.sendUnreliableMessage(mGoogleApiClient, message,
 				mRoom.getRoomId(), idHost);
+	}
+
+	public void sentMyTotalPing(int totalPing){
+		String data = myIndex + "mytotalping" + totalPing;
+		Log.d(TAG, "sentMyTotalPing: " + data);
+		byte[] message = data.getBytes(Charset.forName("UTF-8"));
+		for (Participant p : mParticipants) {
+			if (!p.getParticipantId().equals(mMyId)) {
+				Games.RealTimeMultiplayer.sendUnreliableMessage(mGoogleApiClient, message,
+						mRoom.getRoomId(), p.getParticipantId());
+			}
+		}
 	}
 
 	public synchronized void sentData(int position, String mess, int index) {
@@ -678,10 +811,10 @@ public class TestActivity extends AppCompatActivity
 		}
 		byte[] message = data.getBytes(Charset.forName("UTF-8"));
 		//Games.RealTimeMultiplayer.sendUnreliableMessageToOthers(mGoogleApiClient, message, mRoom.getRoomId());
-		if(myIndex != 0){
+		if(myIndex != hostIndex){
 			Log.d(TAG, "sentData: sent to server");
 			Games.RealTimeMultiplayer.sendUnreliableMessage(mGoogleApiClient, message,
-					mRoom.getRoomId(), mParticipants.get(0).getParticipantId());
+					mRoom.getRoomId(), mParticipants.get(hostIndex).getParticipantId());
 		}else {
 			for (Participant p : mParticipants) {
 				if (!p.getParticipantId().equals(mMyId)) {
@@ -701,7 +834,57 @@ public class TestActivity extends AppCompatActivity
 		}
 	}
 
-	public void test(){
+	public String getCpuInfo() {
+		String value ="";
+		try {
+			Process proc = Runtime.getRuntime().exec("cat /proc/cpuinfo");
+			InputStream is = proc.getInputStream();
+			value = getStringFromInputStream(is);
+		}
+		catch (IOException e) {
+			Log.e(TAG, "------ getCpuInfo " + e.getMessage());
+		}
+		return value;
+	}
 
+	public String getMemoryInfo() {
+		String value = "";
+		try {
+			Process proc = Runtime.getRuntime().exec("cat /proc/meminfo");
+			InputStream is = proc.getInputStream();
+			value = getStringFromInputStream(is);
+		}
+		catch (IOException e) {
+			Log.e(TAG, "------ getMemoryInfo " + e.getMessage());
+		}
+		return value;
+	}
+
+	private static String getStringFromInputStream(InputStream is) {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line = null;
+
+		try {
+			while((line = br.readLine()) != null) {
+				sb.append(line);
+				sb.append("\n");
+			}
+		}
+		catch (IOException e) {
+			Log.e(TAG, "------ getStringFromInputStream " + e.getMessage());
+		}
+		finally {
+			if(br != null) {
+				try {
+					br.close();
+				}
+				catch (IOException e) {
+					Log.e(TAG, "------ getStringFromInputStream " + e.getMessage());
+				}
+			}
+		}
+
+		return sb.toString();
 	}
 }
